2ba4ae7f9372464d4428682aa44565578d3309b4
Sashank <sashankmvv@gmail.com>
2022-04-09 20:58:18 +0530

    added all properties initial

--- property/urls.py
+++ property/urls.py
@@ -3,5 +3,8 @@ from . import views
 urlpatterns = [
 path('home/', views.home, name = 'Home'),
 # path('invest/',views.invest,name='Invest'),
-path('',views.invest,name='Invest'),
+# path('',views.invest,name='Invest'),
+path('allproperties/<str:ctgr>/', views.allprop, name='AllProp'),
+path('invest/<str:property>', views.invest, name='Invest'),
+
 ]
--- property/views.py
+++ property/views.py
@@ -9,19 +9,25 @@ def invest(request):
      labels=[]
      data=[]
      queryset=Property.objects.all()
-     print("queryset",queryset[0])
+ 
      # for investor in queryset:
      #      labels.append(investor.date)
      #      data.append(investor.purchase_price)
      
-     for x in queryset:
-          ps=  ast.literal_eval(x.price_series)
-          ds=  ast.literal_eval(x.date_series)
-          
-          for i in range(len(ps)):
-               data.append(ps[i])
-               labels.append(ds[i])
-     return render(request,'property/invest.html',{
-          'labels':labels,
-          'data':data,
-     })
+
+     ps = ast.literal_eval(queryset.price_series)
+     ds = ast.literal_eval(queryset.date_series)
+
+     for i in range(len(ps)):
+          data.append(ps[i])
+          labels.append(ds[i])
+     return render(request, 'property/invest.html', {
+          'labels': labels,
+          'data': data,
+          })
+
+
+def allprop(request, ctgr):
+    prop = Property.objects.filter(category=ctgr)
+    context = {'prop': prop, }
+    return render(request, 'property/all_properties.html', context)
