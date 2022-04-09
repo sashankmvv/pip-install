ded56e16b38384d4031aa3dd973ed2a10085e3b0
ishadhabalia <isha.dhabalia@somaiya.edu>
2022-04-09 18:43:43 +0530

    Initial chart.js

--- property/urls.py
+++ property/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
 path('', views.home, name = 'Home'),
+path('invest/',views.invest,name='Invest'),
 ]
--- property/views.py
+++ property/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from property.models import Property
 
 def home(request):
      return render(request,'property/home.html')
+
+def invest(request):
+     labels=[]
+     data=[]
+     print("abc")
+     queryset=Property.objects.all()
+     # for investor in queryset:
+     #      labels.append(investor.date)
+     #      data.append(investor.purchase_price)
+     return render(request,'property/invest.html',{
+          'labels':labels,
+          'data':data,
+     })
