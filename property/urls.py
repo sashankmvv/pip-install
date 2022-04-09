102598c655346e19e5855cd0d172b5698a5dca3d
ishadhabalia <isha.dhabalia@somaiya.edu>
2022-04-09 23:01:54 +0530

    Added property views

--- property/urls.py
+++ property/urls.py
@@ -9,7 +9,3 @@ path('allproperties/<str:ctgr>/', views.allprop, name='AllProp'),
 path('invest/<str:property>', views.invest, name='Invest'),
 
 ]
-=======
-path('invest/<str:property>',views.invest,name='Invest'),
-]
->>>>>>> 7fc4b4a82c69424ba87d146c2d1ad66155cf9048
--- property/views.py
+++ property/views.py
@@ -8,18 +8,12 @@ def home(request):
 def invest(request,property):
      labels=[]
      data=[]
-<<<<<<< HEAD
-     queryset=Property.objects.all()
- 
-=======
      queryset=Property.objects.filter(propertyid=property).first()
      
->>>>>>> 7fc4b4a82c69424ba87d146c2d1ad66155cf9048
      # for investor in queryset:
      #      labels.append(investor.date)
      #      data.append(investor.purchase_price)
      
-<<<<<<< HEAD
 
      ps = ast.literal_eval(queryset.price_series)
      ds = ast.literal_eval(queryset.date_series)
@@ -37,16 +31,3 @@ def allprop(request, ctgr):
     prop = Property.objects.filter(category=ctgr)
     context = {'prop': prop, }
     return render(request, 'property/all_properties.html', context)
-=======
-     
-     ps=  ast.literal_eval(queryset.price_series)
-     ds=  ast.literal_eval(queryset.date_series)
-          
-     for i in range(len(ps)):
-          data.append(ps[i])
-          labels.append(ds[i])
-     return render(request,'property/invest.html',{
-          'labels':labels,
-          'data':data,
-     })
->>>>>>> 7fc4b4a82c69424ba87d146c2d1ad66155cf9048
