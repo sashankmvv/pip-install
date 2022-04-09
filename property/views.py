f1981197bcc227e50e361245035928b269a51a2b
ishadhabalia <isha.dhabalia@somaiya.edu>
2022-04-09 19:25:00 +0530

    Rendered data from model in chart

--- property/views.py
+++ property/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from property.models import Property
+import ast
 
 def home(request):
      return render(request,'property/home.html')
@@ -7,11 +8,19 @@ def home(request):
 def invest(request):
      labels=[]
      data=[]
-     print("abc")
      queryset=Property.objects.all()
+     print("queryset",queryset[0])
      # for investor in queryset:
      #      labels.append(investor.date)
      #      data.append(investor.purchase_price)
+     
+     for x in queryset:
+          ps=  ast.literal_eval(x.price_series)
+          ds=  ast.literal_eval(x.date_series)
+          
+          for i in range(len(ps)):
+               data.append(ps[i])
+               labels.append(ds[i])
      return render(request,'property/invest.html',{
           'labels':labels,
           'data':data,
