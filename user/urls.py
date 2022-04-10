d4478fac4498895a300db6fd258922153e517666
mahidedhia <mahi.dedhia@somaiya.edu>
2022-04-10 07:35:40 +0530

    Login module

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "user" 
 
 urlpatterns = [
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login, name="login"),
 ]
