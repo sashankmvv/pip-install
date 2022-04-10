bb7f2411c53c92473ad86b92c7fb0a85bd0b6858
Sashank <sashankmvv@gmail.com>
2022-04-10 06:04:03 +0530

    dynamic home page

--- property/urls.py
+++ property/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-path('invest/', views.home, name = 'Home'),
+path('', views.home, name = 'Home'),
 path('allproperties/<str:ctgr>/', views.allprop, name='AllProp'),
 path('invest/<str:property>', views.invest, name='Invest'),
 
